f5a0d2bf3adf3a72ed4735f6b32d0a6848076615
Laker Newhouse <lakern@mit.edu>
2024-07-17 07:31:12 -0700

    Update progress bar

--- js/progressbar.js
+++ js/progressbar.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (progressBar) {
         progressBar.style.width = '0%';
         requestAnimationFrame(function() {
-            progressBar.style.width = '21.17%';
+            progressBar.style.width = '21.7%';
         });
     }
 });
