c0757037e946268642ef300284a47a33834e45f7
Laker Newhouse <lakern@mit.edu>
2024-07-20 07:04:04 -0700

    Make matching announcement dynamic

--- js/progressbar.js
+++ js/progressbar.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+function getMatchedAmount() {
+    let matchingTotal = 100000;
+    let initialAmountRaised = 127000;
+    let raised = document.getElementById("raised-amount").textContent;
+    raised = raised.replace(/,/g, ''); // remove commas
+    let matchingLeft = (initialAmountRaised + matchingTotal) - parseInt(raised);
+    return parseInt(matchingLeft / 1000);
+}
+
+let progressNotice = "Double Your Impact: The Next $" + getMatchedAmount().toString() + "K You Give Is Matched (US Tax-Deductible)";
+
+document.getElementById("progress-notice").textContent = progressNotice;
+document.documentElement.style.setProperty("--progress-notice-content", "\"" + progressNotice + "\"");
